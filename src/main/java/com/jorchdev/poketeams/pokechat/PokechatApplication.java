a9b5dd615d7bece4e854fef71359868dafdb3b44
JorchDev <jorge.cotera.dev@gmail.com>
2026-08-25 23:59:49 -0600

    core(Init) Initializing repository

---
+++ src/main/java/com/jorchdev/poketeams/pokechat/PokechatApplication.java
@@ -0,0 +1,13 @@
+package com.jorchdev.poketeams.pokechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorchdev/poketeams/pokechat/PokechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorchdev.poketeams.pokechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
